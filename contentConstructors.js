c34ffba0a077502b17038221271bf59da9ac0c67
Thomas Grimes <thomasgrimes@Thomass-MacBook-Pro.local>
2017-11-20 23:19:56 -0500

    Second Commit

--- contentConstructors.js
+++ contentConstructors.js
@@ -3,42 +3,83 @@ function appendElements(element) {
   id = element.id;
   info = element.info;
 
-  if(id === 'skills') {
-    skills(element);
-  } else if (id === 'education') {
-    education(element);
-  } else if (id === 'projects') {
-    projects(element);
-  } else if (id === 'clientReviews') {
-    clientReviews(element);
-  } else if (id === 'references') {
-    references(element);
-  }
-
   function skills () {
     var skills = $('.skills');
     for (var i = 0; i < info.length; i++) {//This loop will construct each skill content
       skills.append('<h3 class="subHeader">'+info[i].skillName+'</h3>');//Append the header
       skills.append('<p class="description">'+info[i].experience+'</p>');//Append the description
       skills.append('<p class="additional">'+info[i].additional+'</p>');//Append the description
+      arrow(skills,i);
     }
   }
 
-  function projects () {
+  function experience () {
+    var experience = $('.experience');
+    for (var i = 0; i < info.length; i++) {
+      experience.append('<h3 class="subHeader">'+info[i].title+'<span class="dates">'+ info[i].dates +'</span></h3>');
+      experience.append('<span><p class="description">'+info[i].description+'</p></span>');
+      arrow(experience,i);
+    }
   }
 
   function education () {
     var education = $('.education');
-    education.append('<h1>' + info[1] + '</h1>')
+    for (var i = 0; i < info.length; i++) {
+      education.append('<h3 class="subHeader">'+info[i].title+'<span class="dates">'+ info[i].dates +'</span></h3>');
+      education.append('<p class="sub2Header">' + info[i].school +' - ' + info[i].cert + '</p>');
+      education.append('<p class="description">' + info[i].description + '</p>')
+      for (var x = 0; x < info[i].skillsLearned.length; x++) {
+        education.append('<li class="listElements">' + info[i].skillsLearned[x] + '</li>')
+      }
+      arrow(education,i);
+    }
   }
 
-  function clientReviews () {
+  function projects (element) {
+    //arrow (projects,i);
     console.log(element);
   }
 
-  function references (element) {
-    console.log(element);
+  function clientReviews (element) {
+    var clientReviews = $('.clientReviews');
+    for (var i = 0; i < info.length; i++) {
+      clientReviews.append('<h3 class="subHeader">'+info[i].name+'</h3>');
+      clientReviews.append('<span class="sub2Header">' +rating(i) + ' - ' + '<span><em>'+info[i].testimonial+ '</em></span></span>');
+      arrow(clientReviews,i);
+    }
   }
 
+  function arrow (element,iterator) {
+    if (iterator+1 != info.length) {
+      element.append('<hr>');
+    } else if (iterator+1 === info.length) {
+        arrow = element.append('<h2 class="upArrow">^</h2>');
+        $('.upArrow').click(function() {
+          ViewModel.toggleVisibility(element);
+        })
+        element.append('<hr>');
+      }
+  }
 
+  function rating(iterator) {
+    star = '<span>&starf;</span>';
+    starList = [];
+    for (var x = 0; x < info[iterator].rating; x++) {
+      starList.push(star);
+    }
+    result = starList.join('');
+    return(result);
+  }
+
+  if(id === 'skills') {
+    skills (element);
+  } else if (id === 'experience') {
+    experience (element);
+  } else if (id === 'education') {
+    education(element);
+  } else if (id === 'projects') {
+    projects (element);
+  } else if (id === 'clientReviews') {
+    clientReviews (element);
+  }
 }
--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 var data = [
   {
-    title : 'Skills',
+    title : 'Language Skills',
     id : 'skills',
     info : [
       {
@@ -10,13 +10,13 @@ var data = [
       'clients demanding advanced functionality to their business operating systems.',
       additional : 'See the related work here'
     },{
-      'skillName' : 'Python',
-      'experience' : 'Retrieved data from multiple third party online sources ' +
+      skillName : 'Python',
+      experience : 'Retrieved data from multiple third party online sources ' +
       'as well analyzed the data with various Python packages.',
       additional : 'See the related work here'
     },{
-      'skillName' : 'HTML5',
-      'experience' : 'Developed optimized professional user interfaces with Bootstrap ' +
+      skillName : 'HTML5',
+      experience : 'Developed optimized professional user interfaces with Bootstrap ' +
       'as well as DOM manipulation techniques to deliver high performance, web-responsive ' +
       'applications.',
       additional : 'See the related work here'
@@ -24,23 +24,84 @@ var data = [
   ]
   },
   {
-    title : 'Projects & Experience',
+    title : 'Work Experience',
+    id : 'experience',
+    info : [
+      {
+        title : 'Business & Computer Science Teacher',
+        employer : 'Fort Hamilton High School',
+        dates : 'September 2017 - Present',
+        description : 'Teacher and curriculum developer of computer science at' +
+        ' Fort Hamilton High School, Bay Ridge Brooklyn. The curriculum involves ' +
+        'students developing and applying concepts in Python, Javascript, and HTML.'
+      },{
+        title : 'Computer Science Teacher / Corporate Trainer',
+        employer : 'Program With Us',
+        dates : 'September 2017 - Present',
+        description : 'Teacher, corporate trainer and curriculum developer of paid ' +
+        'Python and Javascript courses.'
+      },{
+        title : 'Software developer (Private Contractor)',
+        employer : 'Self-employed',
+        dates : 'January 2015 - Present',
+        description : 'Delivered various web-based applications to professional ' +
+        'clients in New York City.'
+      }
+    ]
+  },{
+    title : 'Projects',
     id : 'projects',
-    info : []
+    info : [
+      {
+
+      }
+    ]
   },
   {
     title : 'Education',
     id : 'education',
-    info : ['Test Info','Test Info2']
+    info : [
+      {
+        title : 'Front-end Nano Degree',
+        school : 'Udacity',
+        cert : 'Certificate received November 9th, 2017',
+        dates : 'May 2017 - November 2017',
+        description : 'Demonstrated mastery in the field front-end web development. Skills developed include',
+        skillsLearned : [
+          'Javascript closure techniques',
+          'Feedreader Testing',
+          'API development (Google, Zomato, NY Times among more)',
+          '2D Game engine development',
+          'Javascript code optimization',
+          'MVVM pattern utilizing KnockoutJS',
+          'Bootstrap framework for web responsive applications'
+        ]
+      },{
+        title : 'Python Nano Degree',
+        school : 'Udacity',
+        cert : 'Certificate received May 22nd, 2017',
+        dates : 'March 2017 - May 2017',
+        description : 'Demonstrated mastery in the Python language. Skills developed include:',
+        skillsLearned : ['Webscraping', 'Python OS', 'Test skill 3']
+      },{
+        title : 'Masters in Business Education',
+        school : 'Hofstra University',
+        cert : 'Certificate received May 15th, 2015',
+        dates : 'January 2013 - May 2015',
+        description : 'New York State certified educator grades K-12. GPA - 4.0',
+        skillsLearned : []
+      }
+    ]
   },
   {
-    title : 'Client Reviews',
+    title : 'Client Reviews / References',
     id : 'clientReviews',
-    info : []
-  },
-  {
-    title : 'References',
-    id : 'references',
-    info : []
-  },
+    info : [
+      {
+        name : '544 West 157th Street Housing Development Fund Corporation',
+        rating : 5,
+        testimonial : '"We would recommend Thomas Grimes highly!"'
+      }
+    ]
+  }
 ];
